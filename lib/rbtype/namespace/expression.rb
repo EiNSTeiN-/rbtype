8d1d71434655387791aa867b009b5b3c37eeff35
Francois Chagnon <francois.chagnon@shopify.com>
2018-03-04 12:10:16 -0500

    add missign class

---
+++ lib/rbtype/namespace/expression.rb
@@ -0,0 +1,35 @@
+module Rbtype
+  module Namespace
+    class Expression
+      def initialize(node)
+        raise ArgumentError, "node is nil" unless node
+        @node = node
+      end
+
+      def self.from_node(node)
+        new(node)
+      end
+
+      def type
+        @node.type
+      end
+
+      def const?
+        type == :const || type == :cbase
+      end
+
+      def const_reference
+        ConstReference.from_node(@node) if const?
+      end
+
+      def to_s
+        loc = @node.location.expression
+        "(expression at #{loc.source_buffer.name}:#{loc.line})"
+      end
+
+      def inspect
+        "#<#{self.class.name} #{self}>"
+      end
+    end
+  end
+end
